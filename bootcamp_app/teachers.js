b948f94fb48122ff2ef0092b4f7a385d5fcc422a
vagrant <nabilahmed98@hotmail.com>
2022-09-22 01:08:27 +0000

    updated students.js and teachers.js

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,18 +7,23 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
+const userInput = process.argv.slice(2);
+const cohortArg = userInput[0];
+const queryString = `
+SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+FROM teachers
+JOIN assistance_requests ON teacher_id = teachers.id
+JOIN students ON student_id = students.id
+JOIN cohorts ON cohort_id = cohorts.id
+WHERE cohorts.name LIKE $1
+ORDER BY teacher;
+`;
+const values = [`%${process.argv[2]}%`];
 
-// pool.query(`
-// SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
-// FROM teachers
-// JOIN assistance_requests ON teacher_id = teachers.id
-// JOIN students ON student_id = students.id
-// JOIN cohorts ON cohort_id = cohorts.id
-// WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
-// ORDER BY teacher;
-// `)
-// .then(res => {
-//   res.rows.forEach(row => {
-//     console.log(`${row.cohort}: ${row.teacher}`);
-//   })
-// });
+pool.query(queryString, values)
+.then(res => {
+  res.rows.forEach(row => {
+    console.log(`${row.cohort}: ${row.teacher}`);
+  })
+})
+.catch(err => console.error('query error', err.stack));
